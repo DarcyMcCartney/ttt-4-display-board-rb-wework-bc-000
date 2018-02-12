d0035002b57c5990178faba7c567d725ef1358f9
Darcy McCartney <darcy.mccartney@wework.com>
2018-02-12 04:31:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = ["   ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "]
+board = ["   ", "   ", " 0 ", "   ", " X ", "   ", "   ", "   ", "   "]
 
 def display_board(space)
   puts "#{space[0]}|#{space[1]}|#{space[2]}"
