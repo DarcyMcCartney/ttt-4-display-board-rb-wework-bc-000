a7366476b286a7f50cd51ece9f18b1835a777f01
Darcy McCartney <darcy.mccartney@wework.com>
2018-02-12 04:30:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,21 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
-
-def display_board(space)
-  puts "#{space[0]}|#{space[1]}|#{space[2]}"
-  puts "-----------"
-  puts "#{space[3]}|#{space[4]}|#{space[5]}"
-  puts "-----------"
-  puts "#{space[6]}|#{space[7]}|#{space[8]}"
-end
-
-display_board(board)
-
 board = ["   ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   "]
 
-
 def display_board(space)
   puts "#{space[0]}|#{space[1]}|#{space[2]}"
   puts "-----------"
@@ -26,27 +13,3 @@ end
 
 display_board(board)
 
-
-   board = ["   ", "   ", " 0 ", "   ", "   ", "   ", "   ", "   ", "   "]
-
-def display_board(space)
-  puts "#{space[0]}|#{space[1]}|#{space[2]}"
-  puts "-----------"
-  puts "#{space[3]}|#{space[4]}|#{space[5]}"
-  puts "-----------"
-  puts "#{space[6]}|#{space[7]}|#{space[8]}"
-end
-
-display_board(board)
-
-board = ["   ", "   ", " 0 ", "   ", " X ", "   ", "   ", "   ", "   "]
-
-def display_board(space)
-  puts "#{space[0]}|#{space[1]}|#{space[2]}"
-  puts "-----------"
-  puts "#{space[3]}|#{space[4]}|#{space[5]}"
-  puts "-----------"
-  puts "#{space[6]}|#{space[7]}|#{space[8]}"
-end
-
-display_board(board)
